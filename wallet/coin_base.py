833d37be8f7be5e211c20568dcd7e63a3dbfe1c9
avral <qweasd_96@mail.ru>
2017-08-23 00:28:59 +0300

    refactor coinbase class

--- wallet/coin_base.py
+++ wallet/coin_base.py
@@ -2,7 +2,6 @@ from coinbase.wallet.client import Client
 from dacom_gateway.settings import (
     COIN_BASE_API_KEY,
     COIN_BASE_API_SECRET,
-    COIN_BASE_ACCOUNTS
 )
 
 
@@ -11,8 +10,16 @@ class NotificationError(Exception):
 
 
 class CoinBase(Client):
+    accounts = {}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for account in self.get_accounts()['data']:
+            self.accounts[account['balance']['currency']] = account['id']
+
     def new_address(self, currency):
-        account = self.get_account(COIN_BASE_ACCOUNTS[currency])
+        account = self.get_account(self.accounts[currency])
 
         return account.create_address()['address']
 
